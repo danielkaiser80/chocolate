ae051e5f6f5d411bda8f22f056eaa7ebee2b404a
Daniel Kaiser <danielkaiser@fastmail.com>
2023-10-15 18:13:45 +0200

    Handle the transformation of chocolate elements to be shown correctly.

---
+++ src/backend/chocolates.service.test.ts
@@ -0,0 +1,141 @@
+import { getChocolatesWithInfo } from "./chocolates.service";
+import { describe, expect } from "vitest";
+
+describe("Calculate chocolate information correctly", () => {
+  it("should calculate the cheapest & average prices correctly", () => {
+    const chocolatesWithInfo = getChocolatesWithInfo([
+      {
+        id: "1",
+        name: "Sesam",
+        brand: "Ritter-Sport",
+        currency: "EUR",
+        prices: [
+          {
+            price: 2,
+            shop: "REWE",
+            link: "https://example.com/rs-sesam-re",
+            unit: "g",
+            amount: 200,
+          },
+          {
+            price: 1,
+            shop: "REWE",
+            link: "https://example.com/rs-sesam-xx",
+            unit: "g",
+            amount: 100,
+          },
+        ],
+        nutrition: {
+          fat: {
+            total: 38,
+            saturated: 19,
+          },
+          carbohydrates: {
+            total: 42,
+            sugar: 40,
+          },
+          protein: 11,
+          salt: 1,
+        },
+      },
+      {
+        id: "2",
+        name: "Marzipan",
+        brand: "Ritter-Sport",
+        currency: "EUR",
+        prices: [
+          {
+            price: 1.19,
+            shop: "Ritter-Sport",
+            link: "https://example.com/rs-marzipan-rs",
+            unit: "g",
+            amount: 100,
+          },
+          {
+            price: 1.19,
+            shop: "Amazon",
+            link: "https://example.com/rs-marzipan-am",
+            unit: "g",
+            amount: 100,
+          },
+          {
+            price: 2.19,
+            shop: "REWE",
+            link: "https://example.com/rs-marzipan-re",
+            unit: "g",
+            amount: 200,
+          },
+          {
+            price: 2,
+            shop: "Edeka",
+            link: "https://example.com/rs-marzipan-ed",
+            unit: "kg",
+            amount: 0.2,
+          },
+        ],
+        nutrition: {
+          fat: {
+            total: 28,
+            saturated: 13,
+          },
+          carbohydrates: {
+            total: 53,
+            sugar: 52,
+          },
+          protein: 6,
+          salt: 2,
+        },
+      },
+    ]);
+    expect(chocolatesWithInfo.length).toEqual(2);
+    expect(chocolatesWithInfo).toEqual([
+      {
+        name: "Sesam",
+        brand: "Ritter-Sport",
+        cheapestPricePer100g: 1,
+        cheapestShopLink: "https://example.com/rs-sesam-re",
+        averagePricePer100g: 1,
+      },
+      {
+        name: "Marzipan",
+        brand: "Ritter-Sport",
+        cheapestPricePer100g: 1,
+        cheapestShopLink: "https://example.com/rs-marzipan-ed",
+        averagePricePer100g: 1.11875,
+      },
+    ]);
+  });
+
+  it("should calculate the cheapest & average prices correctly", () => {
+    const chocolatesWithInfo = getChocolatesWithInfo([
+      {
+        id: "1",
+        name: "Sesam",
+        brand: "Ritter-Sport",
+        currency: "EUR",
+        prices: [],
+        nutrition: {
+          fat: {
+            total: 38,
+            saturated: 19,
+          },
+          carbohydrates: {
+            total: 42,
+            sugar: 40,
+          },
+          protein: 11,
+          salt: 1,
+        },
+      },
+    ]);
+    expect(chocolatesWithInfo).toEqual([
+      {
+        name: "Sesam",
+        brand: "Ritter-Sport",
+        cheapestPricePer100g: null,
+        cheapestShopLink: null,
+        averagePricePer100g: null,
+      },
+    ]);
+  });
+});
---
+++ src/backend/chocolates.service.ts
@@ -0,0 +1,48 @@
+import { Chocolate } from "./types.ts";
+
+/**
+ * We 'enhance' an array of chocolate elements, so that we can show it in our app on the starting page.
+ * @param data the chocolate input
+ */
+export const getChocolatesWithInfo = (data: Chocolate[]) =>
+  data.map((chocolate) => ({
+    name: chocolate.name,
+    brand: chocolate.brand,
+    ...findCheapest(chocolate),
+  }));
+
+const findCheapest = (chocolate: Chocolate) => {
+  if (chocolate.prices.length === 0) {
+    // Handle the case when there are no prices.
+    return {
+      cheapestShopLink: null,
+      cheapestPricePer100g: null,
+      averagePricePer100g: null,
+    };
+  }
+
+  const { cheapestPricePer100g, cheapestShopLink, total } =
+    chocolate.prices.reduce(
+      (acc, { amount, link, unit, price }) => {
+        const gramsAmount = unit === "g" ? amount / 100 : amount * 10;
+        const pricePer100g = price / gramsAmount;
+
+        if (pricePer100g < acc.cheapestPricePer100g) {
+          return {
+            cheapestPricePer100g: pricePer100g,
+            cheapestShopLink: link,
+            total: acc.total + pricePer100g,
+          };
+        }
+
+        return { ...acc, total: acc.total + pricePer100g };
+      },
+      { cheapestPricePer100g: Infinity, cheapestShopLink: "", total: 0 },
+    );
+
+  return {
+    cheapestPricePer100g,
+    cheapestShopLink,
+    averagePricePer100g: total / chocolate.prices.length,
+  };
+};
